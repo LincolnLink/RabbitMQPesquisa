34b995d2b6f067140ddd0fc4dc44c6352b92feda
Lincoln <link7-7@hotmail.com>
2026-04-09 11:20:57 -0300

    ajuste no tipo do id

--- API_BASE/Projeto/Projeto.Data/Context/AppDbContext.cs
+++ API_BASE/Projeto/Projeto.Data/Context/AppDbContext.cs
@@ -8,5 +8,17 @@ namespace Projeto.Data.Context
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
         public DbSet<Personagem> Personagens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Personagem>(entity =>
+            {
+                entity.Property(e => e.Id)
+                    .HasColumnType("uuid")
+                    .ValueGeneratedOnAdd();
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
